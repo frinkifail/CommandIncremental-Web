4314cb9eac52acd092e820f3366c6512a27e3f1c
Frinkifail <76682714+frinkifail@users.noreply.github.com>
2023-05-10 15:41:18 +0700

    and let the ChatGPT-generated content begin

---
+++ js/coins.js
@@ -0,0 +1,24 @@
+// Set the maximum number of coins
+const MAX_COINS = 20;
+
+// Function to create a new coin element
+function createCoin() {
+    // Check if the maximum number of coins has been reached
+    if (document.querySelectorAll('.coin').length >= MAX_COINS) {
+        return;
+    }
+    // Create a new coin element
+    let coin = document.createElement('div');
+    coin.classList.add('coin'); // Add the 'coin' class
+    coin.style.position = 'absolute';
+    coin.style.left = Math.random() * window.innerWidth + 'px';
+    coin.style.top = Math.random() * window.innerHeight + 'px';
+    coin.onclick = function () {
+        data.coins += 50;
+        coin.remove(); // Remove the coin element after it's clicked
+    };
+    document.body.appendChild(coin);
+}
+
+// Call the createCoin function every 45 seconds
+setInterval(createCoin, 45000);
