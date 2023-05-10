377a8e3ff633b7171b4af52a323aa025c8607916
Frinkifail <76682714+frinkifail@users.noreply.github.com>
2023-05-10 15:42:38 +0700

    nah 5 seconds better

--- js/coins.js
+++ js/coins.js
@@ -20,5 +20,5 @@ function createCoin() {
     document.body.appendChild(coin);
 }
 
-// Call the createCoin function every 10 seconds
-setInterval(createCoin, 10000);
+// Call the createCoin function every 5 seconds
+setInterval(createCoin, 5000);
