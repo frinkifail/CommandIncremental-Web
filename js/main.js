de778c156f89d159c2308cdd04498962ce5acf57
Frinkifail <76682714+frinkifail@users.noreply.github.com>
2023-05-28 15:51:41 +0700

    bruh seriously i forgor

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function abbreviateNumber(number) {
-    const SI_PREFIXES = ["", "K", "M", "G", "T", "P", "E", "Z", "Y", "X", "W", "V", "U", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A", "Za", "Yi", "Xa", "Wi", "Vi", "Ui", "Ti", "Si", "Ri", "Qi", "Pi", "Oc", "No", "Mo", "Lo", "Ko", "Jo", "Io", "Ho", "Go", "Fo", "Eo", "Do", "Co", "Bo", "Ao", "Zi", "Yu", "Xo", "Wu", "Vu", "Uu", "Tu", "Su", "Ru", "Qu", "Pu", "Ot", "Nt", "Mt", "Lt", "Kt", "Jt", "It", "Ht", "Gt", "Ft", "Et", "Dt", "Ct", "Bt", "At", "Zy", "Yy", "Xy", "Wy", "Vy", "Uy", "Ty", "Sy", "Ry", "Qy", "Py", "Oy", "Ny", "My", "Ly", "Ky"];
+    const SI_PREFIXES = ["", "K", 'M', 'B', 't', 'q', 'Q', 's', 'S', 'o', 'n', 'd', 'U', 'D', 'T', 'Qt', 'Qd', 'Sd', 'St', 'O', 'N', 'v', 'c'];
     const base = Math.floor(Math.log10(Math.abs(number)) / 3);
     const suffix = SI_PREFIXES[base];
     const shortNumber = (number / Math.pow(10, base * 3)).toPrecision(3);
@@ -19,3 +19,6 @@ const update_interval = setInterval(function () {
         e('diamonds-display').textContent = abbreviateNumber(data.diamonds);
     }
 }, 50)
+const s = {};
+e('__nav-home').onclick = () => { data.current_page = 'home'; pagechange(); };
+e('__nav-shop').onclick = () => { data.current_page = 'shop'; pagechange(); };
